05b20ba97a493ddacfc81f36d9faf0cc70a787ff
Rato Bugado <thi23112005ago@gmail.com>
2026-06-08 14:40:55 -0300

    Refactor input prompts and grading output

--- situacao_escolar.java
+++ situacao_escolar.java
@@ -2,44 +2,41 @@ import static java.lang.IO.*;
 
 void main() {
 
-    //Entrada
-
-    println("digite a nota 1: ");
+    println("Digite a nota 1:");
     float nota1 = Float.parseFloat(readln());
 
-    println("digite a nota 2: ");
+    println("Digite a nota 2:");
     float nota2 = Float.parseFloat(readln());
 
-    println("digite a nota 3: ");
+    println("Digite a nota 3:");
     float nota3 = Float.parseFloat(readln());
 
-    println("digite a nota 4: ");
+    println("Digite a nota 4:");
     float nota4 = Float.parseFloat(readln());
 
-    //Processo + Saida
+    float media = (nota1 + nota2 + nota3 + nota4) / 4;
 
-    float media_final = (nota1 + nota2 + nota3 + nota4) / 4;
+    println("\n===== BOLETIM =====");
+    println("Nota 1: " + nota1);
+    println("Nota 2: " + nota2);
+    println("Nota 3: " + nota3);
+    println("Nota 4: " + nota4);
+    println("Média final: " + media);
 
-    println(nota1 + "Nota 1: ");
-    println(nota2 + "Nota 2: ");
-    println(nota3 + "Nota 3: ");
-    println(nota4 + "Nota 4: ");
-    println("Sua media final foi : " + media_final);
-
-    if (media_final <= 3.9) {
-        println("categoria: E");
-        println("voce esta reprovado");
-    } else if (media_final <= 5.9) {
-        println("categoria: D");
-        println("voce esta reprovado");
-    } else if (media_final <= 7.4) {
-        println("categoria: C");
-        println("voce esta aprovado");
-    } else if (media_final <= 8.9) {
-        println("categoria: B");
-        println("voce esta aprovado");
+    if (media <= 3.9) {
+        println("Categoria: E");
+        println("Situação: Reprovado");
+    } else if (media <= 5.9) {
+        println("Categoria: D");
+        println("Situação: Reprovado");
+    } else if (media <= 7.4) {
+        println("Categoria: C");
+        println("Situação: Aprovado");
+    } else if (media <= 8.9) {
+        println("Categoria: B");
+        println("Situação: Aprovado");
     } else {
-        println("categoria: A");
-        println("voce esta aprovado");
+        println("Categoria: A");
+        println("Situação: Aprovado");
     }
 }
